3c36f7b90e2c37f814a679f8fbcc31831c037def
Oscardht <146840169+Oscardht@users.noreply.github.com>
2025-03-21 19:21:15 -0400

    Actualizaciones

--- controller/delete_user.php
+++ controller/delete_user.php
@@ -5,7 +5,7 @@ if(!empty($_GET["id"])){
     $sql = $conn->query(" delete from personas where id=$id ");
 
     if($sql==1){
-        header("location:index.php");
+        header("location:ipodnano.php");
     }else{
         echo "<div class='alert alert-danger'>Error al borrar el usuario</div>";
     }
